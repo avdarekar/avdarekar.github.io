222bc3bd653889d12da641001e62312dd360b300
Ayesha Darekar <62392092+avdarekar@users.noreply.github.com>
2023-08-06 08:05:49 -0400

    Add files via upload

---
+++ assets/js/collapse.js
@@ -0,0 +1,28 @@
+<!-- Code from w3schools.com -->
+
+<!-- Expand and collapse text -->
+var collapse = document.getElementsByClassName("collapse__button");
+var i;
+
+for (i = 0; i < collapse.length; i++) {
+  collapse[i].addEventListener("click", function() {
+
+    this.classList.toggle("active");
+
+
+
+
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+      this.innerText = "Expand";
+
+
+    } else {
+      content.style.display = "block";
+     this.innerText = "Collapse";
+
+    }
+  });
+
+}
