1c4f17f0d2680a53a5c137399b7efd4783f10027
Ayesha Darekar <62392092+avdarekar@users.noreply.github.com>
2025-10-16 18:04:02 -0400

    cleaned up html and js file

--- assets/js/collapse.js
+++ assets/js/collapse.js
@@ -1,28 +1,20 @@
-<!-- Code from w3schools.com -->
+/* Code from w3schools.com */
 
-<!-- Expand and collapse text -->
+/* Expand and collapse text */
 var collapse = document.getElementsByClassName("collapse__button");
 var i;
 
 for (i = 0; i < collapse.length; i++) {
-  collapse[i].addEventListener("click", function() {
-
+  collapse[i].addEventListener("click", function () {
     this.classList.toggle("active");
-
-
-
 
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
       this.innerText = "Expand";
-
-
     } else {
       content.style.display = "block";
-     this.innerText = "Collapse";
-
+      this.innerText = "Collapse";
     }
   });
-
 }
